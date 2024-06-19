22d93722d0477393e86634da477edb66e63f4a9d
MoonSsunny <moonsun116@naver.com>
2024-06-20 01:03:29 +0900

    feat: 라우트 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Seonju from "./pages/Seonju";
+import Shinae from "./pages/Shinae";
+import Sujung from "./pages/Sujung";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/seonju" element={<Seonju />} />
+        <Route path="/msj" element={<Sujung />} />
+        <Route path="/wsa" element={<Shinae />} />
       </Routes>
     </div>
   );
