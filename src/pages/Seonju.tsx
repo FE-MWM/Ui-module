3838fd5fcfb42d5747707a691309f3983d2accf3
MoonSsunny <moonsun116@naver.com>
2024-06-20 01:22:15 +0900

    feat : 예시 기본 페이지 설정

--- src/pages/Seonju.tsx
+++ src/pages/Seonju.tsx
@@ -1,7 +1,23 @@
+import { Link } from "react-router-dom";
+import styles from "./Seonju.module.scss";
+
 const Seonju = () => {
+  const chapter = [
+    { title: "Chapter1", address: "/seonju/chapter1" },
+    { title: "Chapter2", address: "/seonju/chapter2" },
+    { title: "Chapter3", address: "/seonju/chapter3" }
+  ];
+
   return (
-    <div>
-      <h1>선주 Page</h1>
+    <div className={styles.page}>
+      <h1 className={styles.title}>SEONJU</h1>
+      <div className={styles.buttonList}>
+        {chapter.map((item, index) => (
+          <Link to={item.address} key={index}>
+            <button className={styles.chapter}>{item.title}</button>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
