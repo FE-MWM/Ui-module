f0572d4ef47a2b02f29e41a0fceb874aa42f7e44
wooshinae <wooshinae92@gmail.com>
2024-07-03 00:09:41 +0900

    feat: case1_context-menu 구현
    case1_context-menu 구현

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Seonju from "./pages/Seonju";
 import Shinae from "./pages/Shinae";
 import Sujung from "./pages/Sujung";
+import DetailPageShinae from "./pages/DetailPage_Shinae";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/seonju" element={<Seonju />} />
         <Route path="/msj" element={<Sujung />} />
         <Route path="/wsa" element={<Shinae />} />
+        <Route path="/wsa/detail/:id" element={<DetailPageShinae />} />
       </Routes>
     </div>
   );
