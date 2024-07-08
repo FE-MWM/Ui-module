7f4055030ec8d961f09f3d3e7ed99968323ed6ec
moonseonju <sjmoon@glowmee.com>
2024-07-08 17:53:01 +0900

    feat : chapter1. 기능 구현

--- src/components/seonju/case1/Chapter1.tsx
+++ src/components/seonju/case1/Chapter1.tsx
@@ -1,13 +1,22 @@
 import dummyData from "../../../data/seonju/dummyData";
 import Details from "./Details";
 import styles from "./Chapter1.module.scss";
+import { useState } from "react";
 
 const Chapter1 = () => {
+  const [openIndex, setOpenIndex] = useState<number | null>(null);
+  const handleToggle = (index: number) => {
+    setOpenIndex(openIndex === index ? null : index);
+  };
   return (
     <section className={styles.dataList}>
       {dummyData.map((item, index) => (
         <div key={index} className={styles.element}>
-          <Details data={item} />
+          <Details
+            data={item}
+            isOpen={openIndex === index}
+            onToggle={() => handleToggle(index)}
+          />
         </div>
       ))}
     </section>
