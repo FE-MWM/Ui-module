57eef6b971931820b827eece7b2c05261c23acc4
MoonSsunny <moonsun116@naver.com>
2024-08-05 14:48:31 +0900

    feat. 퍼블리싱

--- src/components/seonju/case2/Chapter2.tsx
+++ src/components/seonju/case2/Chapter2.tsx
@@ -1,9 +1,25 @@
+import { useRef, useState } from "react";
+import Nav from "./Nav";
+import Content from "./Content";
 import styles from "./Chapter2.module.scss";
 
+const pages = Array.from({ length: 8 }).map((_, i) => i + 1);
+
 const Chapter2 = () => {
+  const [viewIndex, setViewIndex] = useState<number>(0);
+  const moveToPage = (index: number) => () => {
+    console.log("moveToPage", index);
+  };
+  const contentRef = useRef<(HTMLDivElement | null)[]>([]);
+
   return (
-    <div className={styles.main}>
-      <h1>Chapter2</h1>
+    <div className={styles.app}>
+      <Nav pages={pages} viewIndex={viewIndex} moveToPage={moveToPage} />
+      <div className={styles.contents}>
+        {pages.map((p, i) => (
+          <Content key={p} ref={(r) => (contentRef.current[i] = r)} page={p} />
+        ))}
+      </div>
     </div>
   );
 };
