a42b5441bab65d0377937a5ee08d57ee608faf88
wooshinae <wooshinae92@gmail.com>
2024-09-08 18:06:26 +0900

    feat: case3_InfiniteScroll 기능 구현

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -5,6 +5,7 @@ import Shinae from "../pages/Shinae";
 import Challenge1 from "../components/wsa/Challenge1/Index";
 import Challenge2 from "../components/wsa/Challenge2/Index";
 import Challenge2Origin from "../components/wsa/Challenge2_Origin/Index";
+import Challenge3 from "../components/wsa/Challenge3/Index";
 import Sujung from "../pages/Sujung";
 import React from "react";
 
@@ -25,7 +26,8 @@ const routes: Routes[] = [
   // Shinae
   { path: "/wsa/challenge1", element: <Challenge1 /> },
   { path: "/wsa/challenge2", element: <Challenge2 /> },
-  { path: "/wsa/challenge2_origin", element: <Challenge2Origin /> }
+  { path: "/wsa/challenge2_origin", element: <Challenge2Origin /> },
+  { path: "/wsa/challenge3", element: <Challenge3 /> }
 ];
 
 export default routes;
