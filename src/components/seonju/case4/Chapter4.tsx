dcbf8e8daec7bcf7736af624189d889e975aa73a
sjmoon <sjmoon@glowmee.com>
2024-09-10 11:01:16 +0900

    [feat] UI 수정

--- src/components/seonju/case4/Chapter4.tsx
+++ src/components/seonju/case4/Chapter4.tsx
@@ -1,8 +1,29 @@
+import styles from "./Chapter4.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
+
 const Chapter4 = () => {
   return (
-    <div>
-      <h1>Chapter4</h1>
-    </div>
+    <>
+      <h1 className={styles.title}>Light / Dark Mode</h1>
+      <div className={styles.toggleButton}>
+        <div className={styles.toggleButtonSwitch}></div>
+        <div className={styles.toggleButtonText}>
+          <div className={styles.toggleButtonTextOn}>
+            <FontAwesomeIcon icon={faSun} />
+          </div>
+          <div className={styles.toggleButtonTextOff}>
+            <FontAwesomeIcon icon={faMoon} />
+          </div>
+        </div>
+      </div>
+      <article>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum optio
+        ab porro magni in sunt ipsam, doloremque minima, itaque sapiente
+        consequatur, repellat velit voluptatum accusantium aperiam. Nostrum sunt
+        reprehenderit nemo!
+      </article>
+    </>
   );
 };
 
