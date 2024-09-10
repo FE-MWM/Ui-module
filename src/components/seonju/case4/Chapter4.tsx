f72fe7b892e4bb28d86af1249a8722e7b6b90f0a
sjmoon <sjmoon@glowmee.com>
2024-09-10 14:40:19 +0900

    [feat] darkmode 설정

--- src/components/seonju/case4/Chapter4.tsx
+++ src/components/seonju/case4/Chapter4.tsx
@@ -1,12 +1,27 @@
 import styles from "./Chapter4.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 const Chapter4 = () => {
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
+
+  const handleDarkMode = () => {
+    console.log("checkcheck");
+    const body = document.querySelector("body");
+    if (body?.classList.contains("dark")) {
+      setIsDarkMode(false);
+      body.classList.remove("dark");
+    } else {
+      setIsDarkMode(true);
+      body?.classList.add("dark");
+    }
+  };
+
   return (
     <>
       <h1 className={styles.title}>Light / Dark Mode</h1>
-      <div className={styles.toggleButton}>
+      <div className={styles.toggleButton} onClick={handleDarkMode}>
         <div className={styles.toggleButtonSwitch}></div>
         <div className={styles.toggleButtonText}>
           <div className={styles.toggleButtonTextOn}>
